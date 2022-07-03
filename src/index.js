1c1231aece50ed3c0c0daf074ecf9b6a3470b1fd
Sunil <84917190+sunil-sharma-999@users.noreply.github.com>
2022-07-03 17:30:27 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
